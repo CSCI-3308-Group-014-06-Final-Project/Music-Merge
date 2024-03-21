b41e98e4504cf4ff6001fdad9f034396125769a5
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2024-03-21 11:10:32 -0600

    Begun Work on SQL DB, transfering to Kelly

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+	username VARCHAR(50) PRIMARY KEY,
+	password CHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS playlists (
+	id INT PRIMARY KEY NOT NULL,
+
+);
