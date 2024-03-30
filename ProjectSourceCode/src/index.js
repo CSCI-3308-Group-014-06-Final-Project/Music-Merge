c80a525261713fba6afb39f35642b4c00a23be8d
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2024-03-30 17:22:51 -0600

    Added GET registration and login routes
    - Also added sections to index.js file to section the file
    - Also added a section to have server listen to local port 3000

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -12,3 +12,30 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcrypt'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+// *****************************************************
+// <!-- Section 2 : Connect to DB -->
+// *****************************************************
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+// *****************************************************
+// <!-- Section 4 : API Routes -->
+// *****************************************************
+
+app.get('/login', (req, res) => {
+	res.render('pages/login');
+});
+
+app.get('/register', (req, res) => {
+	res.render('pages/register');
+});
+
+// *****************************************************
+// <!-- Section 5 : Start Server-->
+// *****************************************************
+// starting the server and keeping the connection open to listen for more requests
+app.listen(3000);
+console.log('Server is listening on port 3000');
