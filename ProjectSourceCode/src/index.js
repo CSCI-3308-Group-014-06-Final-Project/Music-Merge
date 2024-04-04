a26cf84c6330dd209389b0b48ad71557457c9b32
Eli Jones <elijones356@gmail.com>
2024-04-04 10:28:44 -0600

    Added app settings, auth middleware, logout page

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -27,14 +27,53 @@ app.use(bodyParser.json()); // specify the usage of JSON for parsing request bod
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
-
+const dbConfig = {
+	host: 'db',
+	port: 5432,
+	database: process.env.POSTGRES_DB,
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+  };
+  const db = pgp(dbConfig);
+  
+  // db test
+  db.connect()
+	.then(obj => {
+	  // Can check the server version here (pg-promise v10.1.0+):
+	  console.log('Database connection successful');
+	  obj.done(); // success, release the connection;
+	})
+	.catch(error => {
+	  console.log('ERROR', error.message || error);
+	});
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
 
+app.use(
+	session({
+	  secret: process.env.SESSION_SECRET,
+	  saveUninitialized: false,
+	  resave: false,
+	})
+  );
 // *****************************************************
 // <!-- Section 4 : API Routes -->
 // *****************************************************
+const user = {
+	password: undefined,
+	username: undefined
+};
+
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
+};
+
+
 app.get('/', (req, res) => {
 	res.render('pages/home')
 });
@@ -65,6 +104,20 @@ app.get('/register', (req, res) => {
 	res.render('pages/register');
 });
 
+
+
+//logout page
+app.get('/logout', auth, (req, res) => {
+    // Destroy the session and logout the user
+    req.session.destroy(err => {
+      if(err) {
+        console.error('Error destroying session:', err);
+      }
+      // Render the logout page with a success message
+      res.render('pages/logout', { message: 'Logged out Successfully' });
+    });
+  });
+  
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
