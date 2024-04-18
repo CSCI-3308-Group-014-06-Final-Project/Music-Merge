05f1f8b4beb1df1eb1fb63da549202827c7dce40
Kelly McVeigh <kellyannemcveigh@gmail.com>
2024-04-18 09:30:26 -0600

    fixed register posts and databases

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,3 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+	spotifyUsername VARCHAR(50) PRIMARY KEY NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS playlists (
 	id INT PRIMARY KEY NOT NULL,
 	spotifyUsername VARCHAR(50)
