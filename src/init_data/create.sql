94c16d69ba3a27b0d7f166d87bca8364314e6b59
Kelly McVeigh <kellyannemcveigh@gmail.com>
2024-04-18 08:59:07 -0600

    removed user database, made small edits to playlists database.

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,12 +1,5 @@
-CREATE TABLE IF NOT EXISTS users (
-	username VARCHAR(50) PRIMARY KEY,
-	password CHAR(60) NOT NULL,
-	spotifyUsername VARCHAR(50)
-);
-
 CREATE TABLE IF NOT EXISTS playlists (
 	id INT PRIMARY KEY NOT NULL,
-	username VARCHAR(50), 
-	FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+	spotifyUsername VARCHAR(50)
 	---> will probably need to link to the playlist using spotify api. not sure what that will look like.
 );
