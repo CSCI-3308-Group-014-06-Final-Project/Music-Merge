06ae998760a5a54942c05b9a6b200f8b2b21633e
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2024-04-23 17:54:53 -0600

    Added REDIRECT_URI env variable

--- src/index.js
+++ src/index.js
@@ -123,7 +123,7 @@ async function redirectToAuthCodeFlow(clientId) {
 	const params = new URLSearchParams();
 	params.append("client_id", clientId);
 	params.append("response_type", "code");
-	params.append("redirect_uri", "http://localhost:3000/login");
+	params.append("redirect_uri", process.env.REDIRECT_URI);
 	params.append("scope", "user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private");
 	params.append("code_challenge_method", "S256");
 	params.append("code_challenge", challenge);
@@ -158,7 +158,7 @@ async function getAccessToken(clientId, code) {
 	params.append("client_id", clientId);
 	params.append("grant_type", "authorization_code");
 	params.append("code", code);
-	params.append("redirect_uri", "http://localhost:3000/login");
+	params.append("redirect_uri", process.env.REDIRECT_URI);
 	params.append("code_verifier", verifier);
 
 	const result = await fetch("https://accounts.spotify.com/api/token", {
